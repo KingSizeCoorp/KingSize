84f98827571411e6bec747143f181da3b323c579
Movinghead333 <Movinghead333@users.noreply.github.com>
2018-06-06 02:04:05 +0200

    VC:3 Version: 1.0.3 -ShowMyCardsActitivty WIP -AddCustomCardActivity WIP

---
+++ app/src/main/java/com/movinghead333/kingsize/AddCustomCardActivity.java
@@ -0,0 +1,13 @@
+package com.movinghead333.kingsize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCustomCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_custom_card);
+    }
+}
--- app/src/main/java/com/movinghead333/kingsize/ShowMyCardsActivity.java
+++ app/src/main/java/com/movinghead333/kingsize/ShowMyCardsActivity.java
@@ -1,10 +1,13 @@
 package com.movinghead333.kingsize;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 public class ShowMyCardsActivity extends AppCompatActivity {
 
@@ -21,6 +24,7 @@ public class ShowMyCardsActivity extends AppCompatActivity {
             @Override
             public void onItemClick(View view, int position) {
                 //TODO: show the clicked card
+                Toast.makeText(getApplicationContext(), "Card with index "+position, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -29,5 +33,14 @@ public class ShowMyCardsActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShowMyCardsActivity.this, AddCustomCardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
