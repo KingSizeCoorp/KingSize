1cc02357c3f5e610027b1505c32ad6013cf4df00
Movinghead333 <Movinghead333@users.noreply.github.com>
2018-06-21 22:55:46 +0200

    replaced CardDetails with Card obejct from database

--- app/src/main/java/com/movinghead333/kingsize/ui/myfeed/CardAdapter.java
+++ app/src/main/java/com/movinghead333/kingsize/ui/myfeed/CardAdapter.java
@@ -9,6 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.movinghead333.kingsize.R;
+import com.movinghead333.kingsize.data.database.Card;
+
 import java.util.List;
 
 import static com.movinghead333.kingsize.R.layout.empoyee_row;
@@ -18,16 +20,16 @@ import static com.movinghead333.kingsize.R.layout.empoyee_row;
  * Created by Abhi on 03 Sep 2017 008.
  */
 
-public class CardAdapter extends ArrayAdapter<CardDetails> {
+public class CardAdapter extends ArrayAdapter<Card> {
     private static final String KEY_ID = "Id: ";
     private static final String KEY_TYPE = "Type: ";
     private static final String KEY_TITLE= "Title: ";
     private static final String KEY_DESCRIPTION = "Description: ";
     private static final String KEY_POSITIVE_VOTES = "Pos. Votes: ";
     private static final String KEY_NEGATIVE_VOTES = "Neg. Votes: ";
-    private List<CardDetails> dataSet;
+    private List<Card> dataSet;
 
-    public CardAdapter(List<CardDetails> dataSet, Context mContext) {
+    public CardAdapter(List<Card> dataSet, Context mContext) {
         super(mContext , R.layout.empoyee_row, dataSet);
         this.dataSet = dataSet;
     }
@@ -41,7 +43,7 @@ public class CardAdapter extends ArrayAdapter<CardDetails> {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = inflater.inflate(R.layout.empoyee_row, null);
         }
-        CardDetails carddetails = dataSet.get(position);
+        Card carddetails = dataSet.get(position);
         if (carddetails != null) {
             //Text View references
             TextView employeeId = (TextView) v.findViewById(R.id.employeeId);
@@ -53,12 +55,12 @@ public class CardAdapter extends ArrayAdapter<CardDetails> {
 
 
             //Updating the text views
-            employeeId.setText(KEY_ID + carddetails.getId());
-            name.setText(KEY_TYPE + carddetails.getType());
-            dob.setText(KEY_TITLE + carddetails.getTitle());
-            designation.setText(KEY_DESCRIPTION + carddetails.getDescription());
-            contactNumber.setText(KEY_POSITIVE_VOTES + carddetails.getPositive_votes());
-            email.setText(KEY_NEGATIVE_VOTES + carddetails.getNegative_votes());
+            employeeId.setText(KEY_ID + carddetails.source);
+            name.setText(KEY_TYPE + carddetails.type);
+            dob.setText(KEY_TITLE + carddetails.title);
+            designation.setText(KEY_DESCRIPTION + carddetails.description);
+            contactNumber.setText(KEY_POSITIVE_VOTES + carddetails.upvotes);
+            email.setText(KEY_NEGATIVE_VOTES + carddetails.downvotes);
         }
 
         return v;
