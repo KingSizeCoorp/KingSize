c07ca33aba45dba0f28eb2271def83f680d6ad24
Movinghead333 <Movinghead333@googlemail.com>
2018-06-22 18:22:42 +0200

    added basic functionality for ViewModel and ViewModelFactory of ShowSingleDeckActivity

--- app/src/main/java/com/movinghead333/kingsize/ui/mydecks/showsingledeckactivity/ShowSingleDeckViewModel.java
+++ app/src/main/java/com/movinghead333/kingsize/ui/mydecks/showsingledeckactivity/ShowSingleDeckViewModel.java
@@ -1,4 +1,18 @@
 package com.movinghead333.kingsize.ui.mydecks.showsingledeckactivity;
 
-public class ShowSingleDeckViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.movinghead333.kingsize.data.KingSizeRepository;
+import com.movinghead333.kingsize.data.database.Card;
+
+public class ShowSingleDeckViewModel extends ViewModel{
+
+    private KingSizeRepository mRepository;
+    private LiveData<Card> cardsInDeck;
+    private long deckId;
+
+    ShowSingleDeckViewModel(KingSizeRepository repository){
+        this.mRepository = repository;
+    }
 }
--- app/src/main/java/com/movinghead333/kingsize/ui/mydecks/showsingledeckactivity/ShowSingleDeckViewModelFactory.java
+++ app/src/main/java/com/movinghead333/kingsize/ui/mydecks/showsingledeckactivity/ShowSingleDeckViewModelFactory.java
@@ -1,4 +1,20 @@
 package com.movinghead333.kingsize.ui.mydecks.showsingledeckactivity;
 
-public class ShowSingleDeckViewModelFactory {
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.movinghead333.kingsize.data.KingSizeRepository;
+
+public class ShowSingleDeckViewModelFactory extends ViewModelProvider.NewInstanceFactory{
+
+    private final KingSizeRepository mRepository;
+
+    public ShowSingleDeckViewModelFactory(KingSizeRepository repository){
+        this.mRepository = repository;
+    }
+
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass){
+        return (T) new ShowSingleDeckViewModel(mRepository);
+    }
 }
